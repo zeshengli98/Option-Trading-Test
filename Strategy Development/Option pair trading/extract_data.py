3a4f99e3d651e57fd5c3b869f471a1d0601b6ea0
zl311 <zl311@duke.edu>
2022-02-27 03:11:54 -0500

    good to extract

--- Strategy Development/Option pair trading/extract_data.py
+++ Strategy Development/Option pair trading/extract_data.py
@@ -9,14 +9,15 @@ stock_symbol = list(us_stock['Symbol'])
 tech_stock = us_stock[us_stock['Sector']=='Technology']
 tech_stock = list(tech_stock['Symbol'])
 
-
+noData = pd.read_csv('noDataRecord.csv')
+noData_list = list(noData.iloc[:,1])
 
 contract = Contract()
 contract.symbol = 'AAPL'
 contract.secType  = 'STK'
 contract.exchange = 'SMART'
 contract.currency = 'USD'
-extracted_data = pd.DataFrame()
+
 noDataRecord = []
 
 historical_data = fetch_historical_data(contract=contract,
@@ -25,10 +26,13 @@ historical_data = fetch_historical_data(contract=contract,
                                         barSizeSetting='1 hour',
                                         whatToShow='MIDPOINT',
                                         useRTH=True)
-
+extracted_data = pd.DataFrame()
 extracted_data['date'] = historical_data['date']
+extracted_data = extracted_data.set_index('date')
 for i, st in enumerate(tech_stock):
     try:
+        if st in noData_list:
+            continue
         contract.symbol = st
         historical_data = fetch_historical_data(contract=contract,
                                                 endDateTime='',
@@ -40,14 +44,17 @@ for i, st in enumerate(tech_stock):
         dataToMerge = historical_data[['date','close']]
 
         dataToMerge = dataToMerge.rename(columns={'close':st})
-        extracted_data = pd.merge(extracted_data, dataToMerge)
+        #extracted_data = pd.merge(extracted_data, dataToMerge)
+        extracted_data = extracted_data.join(dataToMerge.set_index('date'))
+
+        print(extracted_data.head())
         extracted_data.to_csv("stock_data.csv")
         print(f"--------------------------Complete {i} / {len(tech_stock)} stocks extraction!--------------------------")
     except:
         noDataRecord.append(st)
 
 noDataRecord = pd.DataFrame(noDataRecord)
-noDataRecord.to_csv('noDataRecord.csv')
+noDataRecord.to_csv('noDataRecord2.csv')
 
 
 print(extracted_data)
